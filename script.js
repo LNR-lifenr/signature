404fc644949cf515fee9dc4b449973c111b34bf2
LNR-lifenr <60865742+LNR-lifenr@users.noreply.github.com>
2023-11-29 14:07:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-function generateSignature() {
+async function generateSignature() {
     let name = document.getElementById('name').value;
     let title = document.getElementById('title').value;
     let website = document.getElementById('website').value;
     let logo = document.getElementById('logo').files[0];
 
+    // Get signature from your server
+    let response = await fetch('YOUR_SERVER_ENDPOINT');
+    let data = await response.json();
+    let signature = data.signature;
+    let timestamp = data.timestamp;
+
+    // Use Cloudinary's upload API
+    let formData = new FormData();
+    formData.append('file', logo);
+    formData.append('timestamp', timestamp);
+    formData.append('api_key', '822413918619826');
+    formData.append('signature', signature);
+
+    let uploadResponse = await fetch(`https://api.cloudinary.com/v1_1/hilnr/image/upload`, {
+        method: 'POST',
+        body: formData
+    });
+
+    let uploadData = await uploadResponse.json();
+    let logoUrl = uploadData.secure_url; // URL of the uploaded image
+
     // Build the signature HTML
-    let signatureHtml = `<div><img src="${logo}" alt="Logo"><p>${name}<br>${title}<br><a href="${website}">Website</a></p></div>`;
+    let signatureHtml = `<div><img src="${logoUrl}" alt="Logo"><p>${name}<br>${title}<br><a href="${website}">Website</a></p></div>`;
 
     // Display the signature
     document.getElementById('signature-preview').innerHTML = signatureHtml;
